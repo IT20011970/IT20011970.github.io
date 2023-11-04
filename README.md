d99eefa3d82961a2e400990fdd4cf2930742e7f5
Bamunusingha G.P <71841314+IT20011970@users.noreply.github.com>
2023-11-04 12:17:08 +0530

    11

--- README.md
+++
@@ -1,2 +0,0 @@
-# IT20011970.github.io
-TeaBot site
--- assets/js/multi-steps.js
+++
@@ -1,4 +0,0 @@
-$('.wizard li').click(function() {
-  $(this).prevAll().addClass("completed");
-  $(this).nextAll().removeClass("completed")
-});
